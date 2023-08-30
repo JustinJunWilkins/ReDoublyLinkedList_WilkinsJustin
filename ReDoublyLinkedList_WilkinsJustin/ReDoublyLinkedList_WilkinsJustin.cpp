322ec8b6aca7fd31e746e9cc72fc84ad5471b9e5
ジャスティン ウィルキンス <jjustin0318@gmail.com>
2023-08-30 16:02:20 +0900

    プロジェクト ファイルを追加します。

---
+++ ReDoublyLinkedList_WilkinsJustin/ReDoublyLinkedList_WilkinsJustin.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include "class.h"
+using namespace std;
+int main() {
+    DoublyLinkedList scorelist;
+    const string filename = "Scores.txt";
+
+    ifstream ifs(filename);
+
+    if (!ifs) {
+        cout << "file not opened\n";
+        return 0;
+    }
+    int score;
+    string username;
+    while (ifs >> score >> username) {
+        scorelist.insert(scorelist.end(), score, username);
+        cout << score << "  " << username << "\n";
+    }
+    cout << "Data size:\n" << scorelist.getSize() << "\n";
+    ifs.close();
+    return 0;
+}
